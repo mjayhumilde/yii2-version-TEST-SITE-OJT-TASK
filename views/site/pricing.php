3a993909d01ef295a9021e484b8e19a8b0b09444
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-08-07 15:50:26 +0800

    finish creating the whole Pricing Page UI

---
+++ views/site/pricing.php
@@ -0,0 +1,186 @@
+ <?php
+
+    /** @var yii\web\View $this */
+
+    use yii\helpers\Html;
+
+    $this->title = 'Pricing';
+    // $this->params['breadcrumbs'][] = $this->title;
+    ?>
+
+ <section class="mt-5">
+     <div class="container px-4 py-5 px-sm-3 p-sm-5">
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3">
+             <div class="col">
+                 <div class="shadow-sm card border-0 shadow-sm">
+                     <div
+                         class="card-header fw-bold d-flex justify-content-center align-items-center py-3 border-0 text-muted">
+                         Free
+                     </div>
+                     <div class="card-body d-flex flex-column justify-content-center align-items-center text-muted">
+                         <h3 class="card-title d-flex text-success">
+                             <div class="text-start fw-bold p-1" style="font-size: 12px">
+                                 $
+                             </div>
+                             0
+                             <div class="mt-3 text-muted">
+                                 <p style="font-size: 15px">/ month</p>
+                             </div>
+                         </h3>
+                         <p>lroemfs</p>
+                         <p>feres</p>
+                         <p>loremt ipsum</p>
+                         <p>try del tag</p>
+                         <p>try try del</p>
+                     </div>
+                     <div class="card-footer d-flex justify-content-center align-content-end py-3 border-0">
+                         <button class="py-1 px-4 fw-bold text-white border-0 bg-success rounded">
+                             Buy Now
+                         </button>
+                     </div>
+                 </div>
+             </div>
+             <div class="col">
+                 <div class="shadow-sm card border-0 shadow-sm">
+                     <div
+                         class="card-header fw-bold d-flex justify-content-center align-items-center py-3 border-0 bg-success text-white">
+                         Developer
+                     </div>
+                     <div class="card-body d-flex flex-column justify-content-center align-items-center text-muted">
+                         <h3 class="card-title d-flex text-success">
+                             <div class="text-start fw-bold p-1" style="font-size: 12px">
+                                 $
+                             </div>
+                             19
+                             <div class="mt-3 text-muted">
+                                 <p style="font-size: 15px">/ month</p>
+                             </div>
+                         </h3>
+                         <p>lroemfs</p>
+                         <p>feres</p>
+                         <p>loremt ipsum</p>
+                         <p>try del tag</p>
+                         <p>try try del</p>
+                     </div>
+                     <div class="card-footer d-flex justify-content-center align-content-end py-3 border-0">
+                         <button class="py-1 px-4 fw-bold text-white border-0 bg-success rounded">
+                             Buy Now
+                         </button>
+                     </div>
+                 </div>
+             </div>
+             <div class="col">
+                 <div class="shadow-sm card border-0 shadow-sm">
+                     <div
+                         class="card-header fw-bold d-flex justify-content-center align-items-center py-3 border-0 border-0 text-muted">
+                         Business
+                     </div>
+                     <div class="card-body d-flex flex-column justify-content-center align-items-center text-muted">
+                         <h3 class="card-title d-flex text-success">
+                             <div class="text-start fw-bold p-1" style="font-size: 12px">
+                                 $
+                             </div>
+                             29
+                             <div class="mt-3 text-muted">
+                                 <p style="font-size: 15px">/ month</p>
+                             </div>
+                         </h3>
+                         <p>lroemfs</p>
+                         <p>feres</p>
+                         <p>loremt ipsum</p>
+                         <p>try del tag</p>
+                         <p>try try del</p>
+                     </div>
+                     <div class="card-footer d-flex justify-content-center align-content-end py-3 border-0">
+                         <button class="py-1 px-4 fw-bold text-white border-0 bg-success rounded">
+                             Buy Now
+                         </button>
+                     </div>
+                 </div>
+             </div>
+             <div class="col">
+                 <div class="shadow-sm card border-0 shadow-sm">
+                     <div
+                         class="card-header fw-bold d-flex justify-content-center align-items-center py-3 border-0 text-muted">
+                         Ultimate
+                     </div>
+                     <div class="card-body d-flex flex-column justify-content-center align-items-center text-muted">
+                         <h3 class="card-title d-flex text-success">
+                             <div class="text-start fw-bold p-1" style="font-size: 12px">
+                                 $
+                             </div>
+                             49
+                             <div class="mt-3 text-muted">
+                                 <p style="font-size: 15px">/ month</p>
+                             </div>
+                         </h3>
+                         <p>lroemfs</p>
+                         <p>feres</p>
+                         <p>loremt ipsum</p>
+                         <p>try del tag</p>
+                         <p>try try del</p>
+                     </div>
+                     <div class="card-footer d-flex justify-content-center align-content-end py-3 border-0">
+                         <button class="py-1 px-4 fw-bold text-white border-0 bg-success rounded">
+                             Buy Now
+                         </button>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+
+ <section>
+     <div class="bg-light">
+         <div class="container py-5 p-md-5">
+             <div class="d-flex flex-column justify-content-center align-items-center mb-4 text-center">
+                 <h2 class="fw-semibold m-0 mb-2">FREQUENTLY ASKED QUESTIONS</h2>
+                 <div class="d-flex justify-content-center mb-3">
+                     <div class="bg-success mt-2" style="width: 50px; height: 3px;"></div>
+                 </div>
+             </div>
+
+             <div class="px-2 px-sm-3 px-md-5 text-muted">
+                 <div class="accordion gap-2" id="accordionPanelsStayOpenExample">
+                     <div class="accordion-item border-0 mb-3 rounded">
+                         <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                             <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                 <i class="text-secondary me-1 fa-regular fa-circle-question"></i> Accordion Item #1
+                             </button>
+                         </h2>
+                         <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                             <div class="accordion-body">
+                                 <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                             </div>
+                         </div>
+                     </div>
+                     <div class="accordion-item border-0 mb-3 rounded ">
+                         <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                 <i class="text-secondary me-1 fa-regular fa-circle-question"></i> Accordion Item #2
+                             </button>
+                         </h2>
+                         <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                             <div class="accordion-body">
+                                 <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                             </div>
+                         </div>
+                     </div>
+                     <div class="accordion-item  border-0 mb-3 rounded">
+                         <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                                 <i class="text-secondary me-1 fa-regular fa-circle-question"></i> Accordion Item #3
+                             </button>
+                         </h2>
+                         <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                             <div class="accordion-body">
+                                 <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
